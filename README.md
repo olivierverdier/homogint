8b24c764020bfe25055719acc76bc2ccc938d5fd
Olivier Verdier <olivier.verdier@gmail.com>
2015-01-20 21:56:28 +0100

    readme, license, setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Integrators on homogeneous spaces
+
+[![Build Status](https://travis-ci.org/olivierverdier/homogint.svg?branch=master)](https://travis-ci.org/olivierverdier/homogint)
+
+This is a proof-of-concept implementation of the general description of Runge–Kutta on homogeneous spaces, from the paper ["Integrators on homogeneous spaces: Isotropy choice and connections"](http://arxiv.org/abs/1402.6981).
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+
+from distutils.core import setup
+
+setup(
+	name         = 'homogint',
+	version      = '0.1',
+	description  = 'Integration on homogeneous spaces',
+	author = 'Olivier Verdier',
+	url = 'https://github.com/olivierverdier/homogint',
+	license      = 'MIT',
+	keywords = ['Math', 'Homogeneous space',],
+	packages=['homogint',],
+	classifiers = [
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: MIT License',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Scientific/Engineering :: Mathematics',
+	],
+	)
