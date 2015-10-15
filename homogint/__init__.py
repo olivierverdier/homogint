0e971223e16bb54847443a420a2f3838515bbf9b
Olivier Verdier <olivier.verdier@gmail.com>
2015-10-15 10:44:32 +0200

    relative imports

--- homogint/__init__.py
+++ homogint/__init__.py
@@ -2,6 +2,6 @@
 # coding: UTF-8
 from __future__ import division
 
-from homogint import RungeKutta
-from skeletons import *
-from utils import time_step
+from .homogint import RungeKutta
+from .skeletons import *
+from .utils import time_step
