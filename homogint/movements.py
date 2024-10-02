09268148ec6f2fe34a51b8175b94887b6135229e
Olivier Verdier <olivier.verdier@gmail.com>
2024-10-02 13:26:29 +0200

    TYP: missing types

--- homogint/movements.py
+++ homogint/movements.py
@@ -1,9 +1,15 @@
+from typing_extensions import TypeAlias
+import numpy as np
+from numpy.typing import NDArray
+
 from padexp import Exponential  # type: ignore
+
+Vector: TypeAlias = NDArray[np.float64]
 
 Exp = Exponential(order=16)
 
-def exponential(xi):
+def exponential(xi: Vector) -> Vector:
     """
     Maps a Lie algebra vector to a group element.
     """
-    return Exp(xi)[0]
+    return np.asarray(Exp(xi)[0])
